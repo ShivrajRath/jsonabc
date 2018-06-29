a4efb68aa98e3b7689dfb0fb02e0bcf694bb05d0
Shivraj Rath <shivrajrath@gmail.com>
2018-06-29 08:44:48 -0700

    Adds dist folder for the demo on github pages to run

---
+++ dist/jsonabc.js
@@ -0,0 +1,83 @@
+require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({"jsonabc":[function(require,module,exports){
+/*!
+  JSON ABC | License: MIT.
+*/
+
+module.exports = {
+  sort: sort,
+  sortObj: sortObj,
+  cleanJSON: cleanJSON
+};
+
+// Is a value an array?
+function isArray (val) {
+  return Object.prototype.toString.call(val) === '[object Array]';
+}
+
+// Is a value an Object?
+function isPlainObject (val) {
+  return Object.prototype.toString.call(val) === '[object Object]';
+}
+
+// Sorting Logic (Was: 'sortJSON')
+function sortObj (un, noarray) {
+  noarray = noarray || false;
+
+  var or = {};
+
+  if (isArray(un)) {
+    // Sort or don't sort arrays
+    if (noarray) {
+      or = un;
+    } else {
+      or = un.sort();
+    }
+
+    or.forEach(function (v, i) {
+      or[i] = sortObj(v);
+    });
+  } else if (isPlainObject(un)) {
+    or = {};
+    Object.keys(un).sort(function (a, b) {
+      if (a.toLowerCase() < b.toLowerCase()) return -1;
+      if (a.toLowerCase() > b.toLowerCase()) return 1;
+      return 0;
+    }).forEach(function (key) {
+      or[key] = sortObj(un[key]);
+    });
+  } else {
+    or = un;
+  }
+
+  return or;
+}
+
+// Remove trailing commas
+function cleanJSON (input) {
+  input = input.replace(/,[ \t\r\n]+}/g, '}');
+  input = input.replace(/,[ \t\r\n]+\]/g, ']');
+  return input;
+}
+
+// Sort the JSON (clean, parse, sort, stringify).
+function sort (inputStr, noarray) {
+  var output, obj, r;
+
+  if (inputStr) {
+    try {
+      inputStr = cleanJSON(inputStr);
+      obj = JSON.parse(inputStr);
+      r = sortObj(obj, noarray);
+      output = JSON.stringify(r, null, 4);
+    } catch (ex) {
+      console.error('jsonabc: Incorrect JSON object.', [], ex);
+      // Was: window.alert('Incorrect JSON object');
+      throw ex;
+    }
+  }
+  return output;
+}
+
+// End.
+
+},{}]},{},[]);
